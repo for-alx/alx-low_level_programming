2e70d3b284dc8d0e88f3e4577462ee9b5896168f
Abel <hackingtest017@gmail.com>
2022-09-13 17:08:57 -0400

    Function and nested loop

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int a = 0, b = 98, c = 111, d = 81, e = -10;
-
-int main(void)
+int main(int a = 0, b = 98, c = 111, d = 81, e = -10)
 {
     print_to_98(a);
     print_to_98(b);
