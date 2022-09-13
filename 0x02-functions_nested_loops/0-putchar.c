8c75fb510cdd78cb4887813c6239d63516ad6125
Abel <hackingtest017@gmail.com>
2022-09-13 10:49:18 -0400

    Function and nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,14 @@
-#include "main.h"
-/**
- * main - Entry point
- * Description: display _putchat text
- * Return: 1
- */
-int main(void)
-{
-    c[9] = "_putchat";
-    int i = 0;
-    while (i < )
-    {
-    	_putchar(c[i]);
-		i++;
-    }
-    _putchar('\n')
-    return (0);
-}
 
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
