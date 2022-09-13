53408132bdf4f6aac37f0970c6b8cfaf2d90fb4c
Abel <hackingtest017@gmail.com>
2022-09-13 11:57:33 -0400

    Function and nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: display a-z in lowercase
+ * Return: 1
+ */
+void print_alphabet(void)
+{
+	char start = 'a';
+
+	while (start <= 'z')
+	{
+		__putchar('start');
+		
+		start++;
+	} 
+	return (0);
+}
