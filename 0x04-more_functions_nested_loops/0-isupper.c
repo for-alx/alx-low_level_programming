cea84393a3ec21d8d48f1f7b86b8765e2fc4ed5f
Abel <hackingtest017@gmail.com>
2022-09-15 12:38:55 -0400

    more on function and nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
-* _isupper - check for uppercase letter
-* @c : value to check
-* Return : 1 or 0
-*/
+ * _isupper - check for uppercase letter
+ * @c : value to check
+ * Return : 1 or 0
+ */
 
 int _isupper(int c)
 {
 	return (c >= 65 && c <= 90);
 }
+
+#include "holberton.h"
