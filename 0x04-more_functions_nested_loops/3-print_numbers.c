2b42d551d03cef75e99e72c9ef1dfd1ba7acd04c
Abel <hackingtest017@gmail.com>
2022-09-15 14:00:42 -0400

    more on function and nested loop

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,11 @@
 
 void print_numbers(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i;
+
+	for (i = 48; i <= 57; i++)
 	{
-		_putchar('i');
+		_putchar(i);
 	}
 
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,4 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
-
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-
-#endif
