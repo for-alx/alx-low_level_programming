b77864c99085f13613407f57c135f564896dadee
Abel <hackingtest017@gmail.com>
2022-09-15 13:40:03 -0400

    more on function and nested loop

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* mul - multiplies to integers
-* @a : value 1
-* @b : value 2
-* Return : product
+*mul - multiplies two numbers
+*@a: value one
+*@b: value two
+*Return: product
 */
 
 int mul(int a, int b)
