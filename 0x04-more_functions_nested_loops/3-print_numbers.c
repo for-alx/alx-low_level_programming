250d5f73b453d8bcaff16d00d795aaa2ddc0b510
Abel <hackingtest017@gmail.com>
2022-09-15 13:46:44 -0400

    more on function and nested loop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - print numbers 0 upto 9
+*Return: none(void)
+*/
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
