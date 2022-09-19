1d98d7717936c1bba41078e596d80786176d29ff
Abel <hackingtest017@gmail.com>
2022-09-19 16:42:56 -0400

    pointer, string and array

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,6 +9,6 @@ void puts_half(char *str)
 	int i, len = _strlen(str);
 
 	for (i = ((len - 1) / 2) + 1; i < len; i++)
-		putchar(*(str + i));
-	putchar(10);
+		_putchar(*(str + i));
+	_putchar(10);
 }
