6876877fa191ce106d965024efe21a79532b7dbf
Abel <hackingtest017@gmail.com>
2022-09-19 16:47:11 -0400

    pointer, string and array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_array - prints n elements of an array
+* @a: array
+* @n: number of elements
+* Return: none
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	for (n--; n >= 0; n--, i++)
+	{
+		printf("%d", a[i]);
+		if (n > 0)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+
+}
