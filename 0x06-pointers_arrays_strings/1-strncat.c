be7cb232c63e82e46202965e86d340c4f5420b97
Abel <hackingtest017@gmail.com>
2022-09-21 13:36:33 -0400

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,7 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
-	for (j = 0; dest[j] != '\0'; j++)//5
+	for (j = 0; dest[j] != '\0'; j++)
 	;
 	
 	for (i = 0; i < n && src[i] != '\n'; i++)
