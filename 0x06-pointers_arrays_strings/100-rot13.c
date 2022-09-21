b4576e0161ed211610cceac2620e6d80639b0e50
Abel <hackingtest017@gmail.com>
2022-09-21 14:29:30 -0400

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,44 @@
 #include "main.h"
-/**
- * *rot13 - encodes a string using rot13.
- * @s: int type array pointer
- * Return: encoded
- */
-char *rot13(char *s)
-{
-	int i, ii;
-	char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+/**
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: returns the encoded string
+*/
+
+char *rot13(char *str)
+{
+	int index1, index2;
+
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+											 'G', 'H', 'I', 'J', 'K', 'L',
+											 'M', 'N', 'O', 'P', 'Q', 'R',
+											 'S', 'T', 'U', 'V', 'W', 'X',
+											 'Y', 'Z', 'a', 'b', 'c', 'd',
+											 'e', 'f', 'g', 'h', 'i', 'j',
+											 'k', 'l', 'm', 'n', 'o', 'p',
+											 'q', 'r', 's', 't', 'u', 'v',
+											 'w', 'x', 'y', 'z'};
+
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+											 'T', 'U', 'V', 'W', 'X', 'Y',
+											 'Z', 'A', 'B', 'C', 'D', 'E',
+											 'F', 'G', 'H', 'I', 'J', 'K',
+											 'L', 'M', 'n', 'o', 'p', 'q',
+											 'r', 's', 't', 'u', 'v', 'w',
+											 'x', 'y', 'z', 'a', 'b', 'c',
+											 'd', 'e', 'f', 'g', 'h', 'i',
+											 'j', 'k', 'l', 'm'};
+	while (str[++index1])
 	{
-		for (ii = 0; ii < 54; ii++)
+		for (index2 = 0; index2 < 52; index2++)
 		{
-			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A')) && s[i] == input[ii])
+			if (str[index1] == alphabet[index2])
 			{
-				s[i] = output[ii];
+				str[index1] = rot13key[index2];
 				break;
 			}
 		}
 	}
-	return (s);
+	return (str);
 }
