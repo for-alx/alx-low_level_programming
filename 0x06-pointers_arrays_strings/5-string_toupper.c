6ec9a2df80d324fe34dfd62169e523b1c4901fe6
Abel <hackingtest017@gmail.com>
2022-09-21 14:18:59 -0400

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,17 +2,18 @@
 /**
  * string_toupper - all lowercase letters of a string to uppercase.
  * @str : pointer
- * Return: void
+ * Return: str
 */
 char *string_toupper(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] <= 'z' && str[i] >= 'a')
+		{
 			str[i] -= 32;
+		}
 	}
-
 	return (str);
 }
