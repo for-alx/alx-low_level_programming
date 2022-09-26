d349184c22df850b90bf328b734cc90bec802448
Abel <hackingtest017@gmail.com>
2022-09-26 14:09:50 -0400

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; n > 0 ; i++, n--)
+	while (n)
 	{
-		s[i] = b;
+		s[n - 1] = b;
+		n--;
 	}
-
 	return (s);
+
 }
