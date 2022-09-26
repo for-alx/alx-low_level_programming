84b5d224723f83289bd5f39528074526cf7a623b
Abel <hackingtest017@gmail.com>
2022-09-26 14:26:23 -0400

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,16 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	if (*(s + i) == c)
+	{
 		return (s + i);
-	else
-		return (NULL);
+	}
+	return (0);
 }
