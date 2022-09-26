f22c5af6b81d46e3cb1a46d52f09c839178dc4e2
Abel <hackingtest017@gmail.com>
2022-09-26 14:21:50 -0400

    Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_strchr - fills memory with a constant byte.
+* @s: pointer to put the constant
+* @c: constant
+* Return: Null
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
