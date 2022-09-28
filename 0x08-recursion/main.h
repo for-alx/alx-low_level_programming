4d5cee776e52c24ce2b0dc3de9ca282ba6ad823b
Abel <hackingtest017@gmail.com>
2022-09-28 14:48:34 -0400

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,7 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _sqrt(int n, int x);
 int is_prime(int n, int start);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int last_index(char *s);
+int check(char *s, int start, int end, int pair);
