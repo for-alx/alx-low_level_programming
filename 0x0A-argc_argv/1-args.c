7574d75439688854016700693afb0cf330d9ca25
Abel <hackingtest017@gmail.com>
2022-09-30 14:13:12 -0400

    argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints the number of arguments passed into it
+* @argc: command line argument size
+* @argv: command line argument
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		(void)argc;
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+
+	return(0);
+}
