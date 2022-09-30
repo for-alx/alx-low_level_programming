e847f3b77ed00f7da6f7ff8d626056f718b88dbe
Abel <hackingtest017@gmail.com>
2022-09-30 14:42:05 -0400

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print the result of the multiplication
+* @argc: command line argument size
+* @argv: command line argument
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int result;
+	
+	if (argc == 3)
+	{
+		result = atoiargv[1] * atoi(argv[2]));
+	}
+	printf("%d\n", result);
+	return (0);
+}
