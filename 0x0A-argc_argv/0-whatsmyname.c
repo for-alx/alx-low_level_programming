59222f22545e2bfdaecc215e650bd14b7de9910a
Abel <hackingtest017@gmail.com>
2022-09-30 13:50:44 -0400

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,8 +4,10 @@
 * main - prints its name
 * @argc: command line argument size
 * @argv: command line argument
+* Return: 0
 */
 int main(int argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
