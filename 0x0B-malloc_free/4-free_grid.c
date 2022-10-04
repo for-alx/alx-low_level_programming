8c0aca55b6ac48d2144d266ba15da10d71cd0986
Beab <hackingtest017@gmail.com>
2022-10-04 12:28:31 -0400

    Malloc and Free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,13 +5,12 @@
 *@height:height of the array
 *Return:returns void
 */
+
 void free_grid(int **grid, int height)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < height; i++)
+	for (; i < height; i++)
 		free(grid[i]);
-
 	free(grid);
 }
-
