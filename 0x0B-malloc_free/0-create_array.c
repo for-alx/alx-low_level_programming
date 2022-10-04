5dddd0f0f83861d9d732b781c17020df7983233a
Beab <hackingtest017@gmail.com>
2022-10-04 12:06:56 -0400

    Malloc and Free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* creates an array of chars, and initializes it with a specific char.
+*@size: Size of the array
+*@c: Character to insert
+*Return: pointer to the array, or NULL if it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* _strdup - returns a pointer to a newly allocated space in 
+* memory, which contains a copy of the string given as a parameter
+* @str:String to be copied
+* Return: NULL in case of error, pointer to allocated space
+*/
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; str[size] != '\0'; size++)
+	;
+
+	/*+1 on the size puts the end of string character*/
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+	else
+	{
+		for (; i < size; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
