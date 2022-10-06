36c373501e3c85f20bcb6009d99c80ef19fead29
Beab <hackingtest017@gmail.com>
2022-10-06 14:15:40 -0400

    more on malloc and free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,31 +1,26 @@
 #include "main.h"
-
 /**
-* array_range - creates an array of integers
-* @min: smallest number in the array
-* @max: lagrest value in the array
-*
-* Return: pointer to the address of the memory block
-*/
-
+ * *array_range -  create an array of integers
+ * @min: size of the array
+ * @max: char to initialize
+ * Return: pointer to the array initialized or NULL
+ */
 int *array_range(int min, int max)
 {
-	int *block;
-	int i, j = 0;
+	int *m;
+	int i;
 
 	if (min > max)
 		return (NULL);
-	block = malloc(sizeof(*block) * ((max - min) + 1));
-	if (block != NULL)
-	{
-		for (i = min; i <= max; i++)
-		{
-			block[j] = i;
-			j++;
-		}
-		return (block);
-	}
-	else
+
+	m = malloc((max - min + 1) * sizeof(int));
+
+	if (m == 0)
 		return (NULL);
 
+
+	for (i = 0; min + i <= max; i++)
+		m[i] = min + i;
+
+	return (m);
 }
