f23987bc8d28822d970de9d9d3393550f0a6351c
Beab <hackingtest017@gmail.com>
2022-10-10 15:18:45 -0400

    structures typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <iostream.h>
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+*/
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
