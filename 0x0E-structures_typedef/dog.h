2ee6c6c9c7a1c14dd82475c9969558af754bf0cd
Beab <hackingtest017@gmail.com>
2022-10-10 15:30:07 -0400

    structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,6 @@
-#include <iostream.h>
+#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - structure definition of a dog
  * @name: character string
@@ -8,13 +10,18 @@
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
-}
+};
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
+#endif
