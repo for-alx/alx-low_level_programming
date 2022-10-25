6355103495891b741b5e143331f7ff660bd35b18
Mr-robot <hackingtest017@gmail.com>
2022-10-25 14:09:34 -0400

    More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,44 +1,56 @@
 #include "lists.h"
-#include <stdio.h>
 /**
- * delete_nodeint_at_index - deletes a node at a given position.
- * @head: head of linked list
- * @index: index to delete
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_nodeint_at_index - deletes a node at an index
+ * @head: pointer to the head of the list
+ * @index: index of the node to be added
+ *
+ * Return: the address of the node
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current;
-	listint_t *tmp;
+	listint_t *old_node = NULL;
+	listint_t *previous_node = NULL;
+	unsigned int i = 0, list_len = listint_len(*head);
 
-	if (head == NULL || (*head) == NULL)
+	if ((index > list_len) || (list_len == 0))
 		return (-1);
-
-	current = *head;
-	if (index == 0)
+	while (head != NULL)
 	{
-		if ((*head)->next)
-			(*head) = (*head)->next;
-		else
-			(*head) = NULL;
-		free(current);
-		return (1);
+		if (i == index)
+		{
+			old_node = *head;
+			if (i == 0)
+			{
+				*head = old_node->next;
+				free(old_node);
+				return (1);
+			}
+			previous_node->next = old_node->next;
+			free(old_node);
+			return (1);
+		}
+		else if ((i + 1) == index)
+			previous_node = *head;
+		head = &((*head)->next);
+		i++;
 	}
+	return (-1);
+}
 
-	while (index != 1)
+/**
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
+ * Return: the number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
 	{
-		if (current->next == NULL)
-			return (-1);
-
-		current = current->next;
-		--index;
+		count += 1;
+		cursor = cursor->next;
 	}
-	tmp = current->next;
-	if (current->next->next)
-		current->next = current->next->next;
-	else
-		current->next = NULL;
-	free(tmp);
-
-	return (1);
+	return (count);
 }
