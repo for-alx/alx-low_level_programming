cce5336ae751e82c63c1e0a1f17f912dba7c456c
Mr-robot <hackingtest017@gmail.com>
2022-10-27 14:22:07 -0400

    Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* get_bit - gets bit on index `index`
-* @n: decimal number
-* @index: index of the bit
-* Return: the bit found
+*get_bit - get the value of a bit at the given index
+*@n: the number
+*@index: index starting from 0 of the bit required
+*Return: The converted value
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(n) * BYTE_LENGHT)
+	if (index > 63)
 		return (-1);
 
 	return ((n >> index) & 1);
