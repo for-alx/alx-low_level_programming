b6930fc8c577395c5ecf8385262f9292bf1b08b7
Mr-robot <hackingtest017@gmail.com>
2022-12-08 16:00:52 +0300

    Doubly linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,43 +1,34 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node at specific index
- * @head: head of linked list
- * @index: index of node to delete
- * Return: 1 on success, -1 on failure
+ * delete_dnodeint_at_index - deletes the node at index of a linked list
+ * @head: pointer to the head of the list
+ * @index: of the node te remove
+ * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
+	unsigned int idx = 0;
+	dlistint_t *node;
+	dlistint_t **ptr;
 
-	if (head == NULL)
+	if (!head || !*head)
 		return (-1);
 
-	current = *head;
+	ptr = head;
 
-	if (current == NULL)
+	while (idx < index && (*ptr)->next)
+	{
+		ptr = &((*ptr)->next);
+		idx++;
+	}
+	if (idx < index)
 		return (-1);
 
-	while (index != 0)
-	{
-		current = current->next;
-		index--;
-		if (current == NULL)
-			return (-1);
-	}
-
-	if (current->prev != NULL)
-		current->prev->next = current->next;
-	else
-	{
-		*head = current->next;
-		if (current->next != NULL)
-			current->next->prev = NULL;
-		free(current);
-	}
-
+	if ((*ptr)->next)
+		((*ptr)->next)->prev = (*ptr)->prev;
+	node = *ptr;
+	*ptr = (*ptr)->next;
+	free(node);
 	return (1);
 }
-
-
