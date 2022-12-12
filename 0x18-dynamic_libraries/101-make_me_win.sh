e5d6e73972da798814a8ac5baea4b9ea45d5d4e8
Mr-robot <hackingtest017@gmail.com>
2022-12-12 22:09:23 +0300

    Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+
+wget -P /tmp/ https://github.com/for-alx/alx-low_level_programming/blob/main/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so
