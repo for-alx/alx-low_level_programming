b3e151c8cec7b4846ef1967f0d53c9508ac68920
Mr-robot <hackingtest017@gmail.com>
2023-04-12 20:13:39 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,7 +15,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t start = 0;
 	size_t end = sqrt(size);
 
-	if (array == NULL || size = 0)
+	if (array == NULL || size == 0)
 		return (-1);
 	printf("Value checked array[%lu] = [%d]\n", start, array[start]);
 	while (array[end] < value && start < size)
