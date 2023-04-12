679a67f6aa94a5f89a87ce1b4c30e555fa3b1b57
Mr-robot <hackingtest017@gmail.com>
2023-04-12 20:25:41 +0300

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,11 +12,11 @@
 **/
 int jump_search(int *array, size_t size, int value)
 {
-	size_t = start = 0;
-	size_t = end = 0;
+	size_t start = 0;
+	size_t end = 0;
 	size_t jump = sqrt(size);
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 	while (array[end] < value && end < size)
 	{
